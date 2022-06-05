8e23cd43721bfa487262e4866393ee7e8787fa99
harshit0571 <harshitchadha65@gmail.com>
2022-06-05 08:40:57 +0530

    oops quedtion

---
+++ oops/questions/2.cpp
@@ -0,0 +1,34 @@
+// problem statement-> name and age;
+
+// link->https://www.codingninjas.com/codestudio/guided-paths/basics-of-c/content/118817/offering/1381817
+
+// #include<iostream>
+
+// using namespace std;
+
+// class Person {
+//     string name;
+//     int age;
+//     public:
+//     void SetValue(string a, int b){
+//         name=a;
+//         age=b;
+//     }
+//     void GetValue(){
+//         cout<<"The name of the person is "<<name<<" and the age is "<<age<<".";
+//     }
+
+// };
+
+// int main() {
+
+//     //Write your code here
+//     string a;
+//     int b;
+//     cin>>a>>b;
+//     Person obj1;
+//     obj1.SetValue(a,b);
+//     obj1.GetValue();
+
+//     return 0;
+// }
