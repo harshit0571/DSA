e50e43242f20f9de10182301ed58cf04e1152cb8
harshit0571 <harshitchadha65@gmail.com>
2022-06-12 13:31:52 +0530

    exponential of four

---
+++ bitwise_operators/check_power_of_4.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+using namespace std;
+int check(int n)
+{
+    int count = 0;
+    if (!(n & (n - 1)))
+    {
+        while (n > 1)
+        {
+            n >>= 1;
+            count++;
+        }
+    }
+    else
+    {
+        count = 3;
+    }
+    return count;
+}
+int main()
+{
+    int x;
+    cin >> x;
+    int count = check(x);
+    if (count % 2 == 0)
+    {
+        cout << "exponential of four" << count;
+    }
+    else
+    {
+        cout << "not " << count;
+    }
+}
