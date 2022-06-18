23ac9f1a4f2d7d6d5c9d0b83aec522390e1356f7
harshit0571 <harshitchadha65@gmail.com>
2022-06-18 14:01:44 +0530

    leetcode easy questions array

--- arrays/find_unique_element.cpp
+++ arrays/find_unique_element.cpp
@@ -2,6 +2,13 @@
 #include <vector>
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
-    
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5, 5, 2, 1, 4};
+    int temp = 0;
+    for (int i = 0; i < 9; i++)
+    {
+        temp = temp ^ arr[i];
+    }
+    cout << temp;
 }
