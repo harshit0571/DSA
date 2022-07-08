4607068836d03990759eb7291cf73a539a18f528
Harshit chadha <62325935+harshit0571@users.noreply.github.com>
2022-07-08 09:29:45 +0530

    Create remove_element.cpp

---
+++ leetcode_questions/easy/remove_element.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int count=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==val){
+                nums[i]=INT_MAX;
+                count++;
+            }
+        }
+        sort(nums.begin(),nums.end());
+        return nums.size()-count;
+        
+    }
+};
