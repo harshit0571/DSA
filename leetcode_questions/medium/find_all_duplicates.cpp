dc9ec7b6e7840a33067cd3eb885fc11462b2d732
Harshit chadha <62325935+harshit0571@users.noreply.github.com>
2022-07-08 09:59:07 +0530

    Create find_all_duplicates.cpp

---
+++ leetcode_questions/medium/find_all_duplicates.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& n) {
+        vector<int> a;
+        int i=0;
+        while(i<n.size()){
+            int current=n[i];
+            if(n[i]!=i+1){
+                if(n[current-1]!=current){
+                    swap(n[i], n[current-1]);
+                }
+                else{
+                    i++;
+                }
+            }
+            else{
+                i++;
+            }
+        }
+        for(int i=0;i<n.size();i++){
+            if(n[i]!=i+1){
+                a.push_back(n[i]);
+            }
+        }
+        return a;
+    }
+};
