fc7b54cab6275b372e8a998f580cae3c4d9bf4eb
Harshit chadha <62325935+harshit0571@users.noreply.github.com>
2022-08-04 14:23:22 +0530

    Update Creating.cpp

--- Linked-List/Creating.cpp
+++ Linked-List/Creating.cpp
@@ -1,1 +1,18 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
 
+class Node{
+  public:
+  int data;
+  Node *next;
+  
+};
+
+int main() {
+    // Write C++ code here
+    Node *node1=new Node();
+    cout<<node1->data<<" "<<node1->next;
+
+    return 0;
+}
