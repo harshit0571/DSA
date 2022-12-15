e7fe51f91c6b69ce5fd5291a82affe8b13b38492
Harshit chadha <62325935+harshit0571@users.noreply.github.com>
2022-12-15 22:34:08 +0530

    Update BST_EVERYTHING.cpp

--- Tree/BST_EVERYTHING.cpp
+++ Tree/BST_EVERYTHING.cpp
@@ -1,97 +1,79 @@
+// Online C++ compiler to run C++ program online
 #include <iostream>
 #include <bits/stdc++.h>
-#include <vector> 
-#include <stack>
-#include <string>
-#include <queue>
 using namespace std;
+
+
 class Node{
     public:
     int data;
     Node* left;
     Node* right;
-    
     Node(int d){
-        this->data = d;
-        this->left = NULL;
-        this->right = NULL;
+        data=d;
+        this->left=NULL;
+        this->right=NULL;
     }
 };
-Node* insertintoBST(Node* &root,int d){
-    if(root == NULL){
-        root = new Node(d);
-        return root;
-    }
-    if(d>root->data){
-        root->right = insertintoBST(root->right,d);
-    }
-    else{
-        root->left = insertintoBST(root->left,d);
-    }
-    return root;
-}
-void takinginput(Node* &root,int n){
-    int data;
-    while(n>0){
-        cin>>data;
-        insertintoBST(root,data);
-        n--;
+void LevelOrder(Node* root){
+    queue<Node*> Q;
+    Node* temp=root;
+    Q.push(temp);
+    while(!Q.empty()){
+        temp=Q.front();
+        Q.pop();
+        cout<<temp->data<<" ";
+        if(temp->left){
+            Q.push(temp->left);
+        }
+        if(temp->right){
+            Q.push(temp->right);
+        }
     }
 }
 void preorder(Node* root){
-    if(root ==NULL){
+    if(root==NULL){
         return;
     }
-    cout<<root->data<<" ";
+    cout<<root->data;
     preorder(root->left);
     preorder(root->right);
 }
 void inorder(Node* root){
-    if(root ==NULL){
+    if(root==NULL){
         return;
     }
     inorder(root->left);
-    cout<<root->data<<" ";
-    inorder(root->right);
+    cout<<root->data;
+    inorder(root->right);    
 }
 void postorder(Node* root){
-    if(root ==NULL){
+    if(root==NULL){
         return;
     }
     postorder(root->left);
-    postorder(root->right);
-    cout<<root->data<<" ";
+    postorder(root->right); 
+        cout<<root->data;
+
 }
-void levelorder(Node* root){
-    queue<Node*>q;
-    q.push(root);
-    while(!q.empty()){
-        Node* temp = q.front();
-        cout<<temp -> data<<" ";
-        q.pop();
-        if(temp->left){
-            q.push(temp->left);
-        }
-        if(temp->right){
-            q.push(temp->right);
-        }
+Node* InsertIntoBST(Node* root, int data){
+    if(root==NULL){
+        root=new Node(data);
+        return root;
     }
+    if(data>root->data){
+        root->right=InsertIntoBST(root->right, data);
+    }
+    else if(data<root->data){
+        root->left=InsertIntoBST(root->left, data);
+    }
+    return root;    
 }
-int main(){
-    int n;
-    cin >>n;
-    Node* root =NULL;
-    takinginput(root,n);
-    cout<<"preorder : ";
-    preorder(root);
-    cout<<endl;
-    cout<<"inorder : ";
-    inorder(root);
-    cout<<endl;
-    cout<<"postorder : ";
-    postorder(root);
-    cout<<endl;
-    cout<<"levelorder : ";
-    levelorder(root);
-    cout<<endl;
+void TakeInput(Node* &root,int n){
+    int data;
+    while(n>0){
+        cin>>data;
+        root=InsertIntoBST(root, data);
+        n--;
+    }
 }
