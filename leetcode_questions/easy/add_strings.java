b3178f8de4cba0b03f617ac40db78ec6aa0c67bd
Harshit chadha <62325935+harshit0571@users.noreply.github.com>
2023-05-15 12:07:53 +0530

    second soln

--- leetcode_questions/easy/add_strings.java
+++ leetcode_questions/easy/add_strings.java
@@ -13,3 +13,56 @@ class Solution {
         return ans.reverse().toString();
     }
 }
+
+
+//second soln
+class Solution {
+    public String addStrings(String num1, String num2) {
+        StringBuilder ans=new StringBuilder();
+        int i1=num1.length()-1;
+        int i2=num2.length()-1;
+        char carr='0';
+        while(i1>=0 && i2>=0){
+            String sum=(num1.charAt(i1) -'0')+ (num2.charAt(i2)-'0') + (carr-'0') +"";
+            if(sum.length()>1){
+                ans.append(sum.charAt(1));
+                carr=sum.charAt(0);
+            }
+            else{
+                ans.append(sum.charAt(0));
+                carr='0';
+            }
+            System.out.println(sum);
+            i1--;
+            i2--;
+        }
+        while(i1>=0){   
+            String sum=(num1.charAt(i1) -'0')+(carr-'0') +"";
+            if(sum.length()>1){
+                ans.append(sum.charAt(1));
+                carr=sum.charAt(0);
+            }
+            else{
+                ans.append(sum.charAt(0));
+                carr='0';
+            }
+            i1--;
+        }
+        while(i2>=0){
+            String sum=(num2.charAt(i2) -'0')+(carr-'0') +"";
+            if(sum.length()>1){
+                ans.append(sum.charAt(1));
+                carr=sum.charAt(0);
+            }
+            else{
+                ans.append(sum.charAt(0));
+                carr='0';
+            }
+            i2--;
+        }
+        if(carr!='0'){
+            ans.append(carr);
+        }
+        return ans.reverse().toString();
+    }
+}
