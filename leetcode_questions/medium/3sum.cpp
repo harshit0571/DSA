04b3650a53eefe1f1eea39e80ff67f8d1297b8cd
Harshit chadha <62325935+harshit0571@users.noreply.github.com>
2023-05-17 18:53:13 +0530

    Update 3sum.cpp using java

--- leetcode_questions/medium/3sum.cpp
+++ leetcode_questions/medium/3sum.cpp
@@ -38,3 +38,46 @@ public:
         
     }
 };
+
+
+
+
+//java
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans=new ArrayList<>();
+        Arrays.sort(nums);
+        int prev=nums[0];
+        for(int i=0;i<nums.length-2;i++){
+            if(prev==nums[i] && i!=0){
+                continue;
+            }
+            int left=i+1;
+            int right=nums.length-1;
+            while(left<right){
+                int sum=nums[i]+nums[left]+nums[right];
+                if(sum==0){
+                    ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    while(nums[left]==nums[left-1] && left<right){
+                        left++;
+                    }
+                }
+                else if(sum>0){
+                    right--;
+                }
+                else{
+                    left++;
+                }
+            }
+            prev=nums[i];
+        }
+        return ans;
+       
+    }
+}
+
+// -1 -1 0 1 2 4
+// -2 0 0 2 2
+
