5358c6dbf6610207bf4113957b63eb1ebdd1f4ea
Harshit chadha <62325935+harshit0571@users.noreply.github.com>
2023-05-17 17:40:58 +0530

    Update Isomorphic_string.java

--- leetcode_questions/easy/Isomorphic_string.java
+++ leetcode_questions/easy/Isomorphic_string.java
@@ -36,3 +36,28 @@ class Solution {
         return ans && ans2;
     }
 }
+
+
+
+
+
+//isomorphic maps
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        HashMap<Character, Character> map=new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            if(map.containsKey(s.charAt(i))){
+                if(t.charAt(i)!=map.get(s.charAt(i))){
+                    return false;
+                }
+            }
+            else{
+                if(map.containsValue(t.charAt(i))){
+                    return false;
+                }
+                map.put(s.charAt(i), t.charAt(i));
+            }
+        }
+        return true;
+    }
+}
