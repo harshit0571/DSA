f214312d3fdce396a69ab264758db0ba8ae75bd2
Harshit chadha <62325935+harshit0571@users.noreply.github.com>
2023-05-18 11:19:59 +0530

    Update valid_anagram.java

--- leetcode_questions/easy/valid_anagram.java
+++ leetcode_questions/easy/valid_anagram.java
@@ -25,3 +25,57 @@ class Solution {
 
     }
 }
+
+
+
+//using array
+class Solution {
+
+    public boolean isAnagram(String a, String b) {
+          char[] a1 = a.toCharArray();
+        char[] a2 = b.toCharArray();
+        Arrays.sort(a1);
+        Arrays.sort(a2);
+        if (a1.length != a2.length) {
+            return false;
+        } else {
+        boolean flag = true;
+        for (int i = 0; i < a2.length; i++) {
+        if (a1[i] != a2[i]) {
+            return false;
+        }
+        }
+        return flag;
+        }
+    }
+}
+
+
+
+//using maps
+class Solution {
+
+    public boolean isAnagram(String a, String b) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        HashMap<Character, Integer> map2 = new HashMap<>();
+        if(a.length()!=b.length()){
+            return false;
+        }
+        for (int i = 0; i < a.length(); i++) {
+            if (map.containsKey(a.charAt(i))) {
+                map.put(a.charAt(i), map.get(a.charAt(i)) + 1);
+            } else {
+                map.put(a.charAt(i), 1);
+            }
+
+            if (map2.containsKey(b.charAt(i))) {
+                map2.put(b.charAt(i), map2.get(b.charAt(i)) + 1);
+            } else {
+                map2.put(b.charAt(i), 1);
+            }
+        }
+
+        return map.equals(map2);
+
+    }
+}
