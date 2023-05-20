d6ae3fe9d8f4eb2492a3879a411fd579f2a377c9
Harshit chadha <62325935+harshit0571@users.noreply.github.com>
2023-05-20 12:58:36 +0530

    Update optimal_partition_string.java using hashset

--- leetcode_questions/medium/optimal_partition_string.java
+++ leetcode_questions/medium/optimal_partition_string.java
@@ -18,3 +18,27 @@ class Solution {
         return count+1;
     }
 }
+
+
+
+//using hashset
+class Solution {
+    public int partitionString(String s) {
+        int count=0;
+        for(int i=0;i<s.length();i++){
+            HashSet<Character> map=new HashSet<>();
+            for(int j=i;j<s.length();j++){
+                if(map.contains(s.charAt(j))){
+                    System.out.println(s.charAt(j) + " "+j);
+                    count++;
+                    i=j-1;
+                    break;
+                }
+                else{
+                    map.add(s.charAt(j));
+                }
+            }
+        }
+        return count+1;
+    }
+}
