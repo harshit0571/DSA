d1d68807f15d2c46f8fc7aa78a1f788bac28a0f9
Harshit chadha <62325935+harshit0571@users.noreply.github.com>
2023-05-21 16:42:45 +0530

    Update intersection_LL.cpp

--- leetcode_questions/easy/intersection.cpp
+++ leetcode_questions/easy/intersection.cpp
@@ -46,3 +46,62 @@ public:
 
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    int length(ListNode t){
+        int count=0;
+        while(t!=null){
+            t=t.next;
+            count++;
+        }
+        return count;
+    }
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        int n1=length(headA);
+        int n2=length(headB);
+        if(headA!=null && n1>n2){
+            int t=n1-n2;
+            while(t>0){
+                headA=headA.next;
+                t--;
+            }
+        }
+        else{
+            int t=n2-n1;
+            while(t>0){
+                headB=headB.next;
+                t--;
+            }
+        }
+        System.out.println(headA.val +" "+headB.val);
+        while(headA !=null && headB!=null){
+                if(headA==headB){
+                    return headA;
+                }
+                headA=headA.next;
+                headB=headB.next;
+        }
+        return null;
+    }
+}
