3da524bdf7d8f8d1485d31c1cae01675270b9ff2
Harshit chadha <62325935+harshit0571@users.noreply.github.com>
2023-05-21 11:57:19 +0530

    Update SetMatrixZeroes.java

--- leetcode_questions/medium/SetMatrixZeroes.java
+++ leetcode_questions/medium/SetMatrixZeroes.java
@@ -26,3 +26,36 @@ class Solution {
         }
     }
 }
+
+
+
+
+
+//using hashSet
+
+class Solution {
+    public void setZeroes(int[][] M) {
+        int n=M.length;
+        int m=M[0].length;
+        HashSet<Integer> rowSet=new HashSet<>();
+        HashSet<Integer> colSet=new HashSet<>();
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(M[i][j]==0){
+                    rowSet.add(i);
+                    colSet.add(j);
+                }
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(rowSet.contains(i) || colSet.contains(j) ){
+                    M[i][j]=0;
+                }
+            }
+        }
+    }
+}
+
