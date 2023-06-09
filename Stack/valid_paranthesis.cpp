f7b5c4b70c0809248c03420c190f6e1744871742
Harshit chadha <62325935+harshit0571@users.noreply.github.com>
2023-06-09 19:46:32 +0530

    Update valid_paranthesis.cpp

--- Stack/valid_paranthesis.cpp
+++ Stack/valid_paranthesis.cpp
@@ -51,3 +51,54 @@ int main()
     check(s);
     
 }
+
+
+
+
+
+
+
+
+
+
+
+//java solution
+
+class Solution {
+    public boolean isValid(String s) {
+        if(s.length()%2!=0){
+            return false;
+        }
+        Stack<Character> st=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i) == '(' || s.charAt(i) == '['  || s.charAt(i) == '{' ){
+                st.push(s.charAt(i));
+            }
+            else if(s.charAt(i) == ')'){
+                if(!st.empty() && st.peek() == '('){
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+            else if(s.charAt(i) == '}'){
+                if(!st.empty() && st.peek() == '{'){
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+            else if(s.charAt(i) == ']'){
+                if(!st.empty() && st.peek() == '['){
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return st.empty();
+    }
+}
