61f6d2ae89053358685039b6f5869d271fcae40f
Harshit chadha <62325935+harshit0571@users.noreply.github.com>
2023-06-15 20:27:45 +0530

    Update search_insert_position.cpp

--- leetcode_questions/easy/search_insert_position.cpp
+++ leetcode_questions/easy/search_insert_position.cpp
@@ -21,3 +21,27 @@ public:
         
     }
 };
+
+
+
+
+//java soln
+class Solution {
+    public int searchInsert(int[] arr, int target) {
+        int left=0;
+        int right=arr.length-1;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(arr[mid] == target){
+                return mid;
+            }
+            else if(arr[mid] > target){
+                right=mid-1;
+            }
+            else{
+                left=mid+1;
+            }
+        }
+        return left;
+    }
+}
