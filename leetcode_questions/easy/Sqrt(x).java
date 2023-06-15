508e203723550bf19545d61aff7a0e5901c41bc7
Harshit chadha <62325935+harshit0571@users.noreply.github.com>
2023-06-15 20:21:43 +0530

    Update Sqrt(x).java

--- leetcode_questions/easy/Sqrt(x).java
+++ leetcode_questions/easy/Sqrt(x).java
@@ -11,3 +11,29 @@ class Solution {
         return ans;
     }
 }
+
+
+
+
+class Solution {
+    public int mySqrt(int x) {
+        if(x==0){
+            return x;
+        }
+        int right=x, left=1;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            System.out.println(mid);
+            if(mid/x==mid){
+                return mid;
+            }
+            else if(mid>x/mid){
+                right=mid-1;
+            }
+            else{
+                left=mid+1;
+            }
+        }
+        return right;
+    }
+}
