8be7b7a9b58c17eb55e92d6cfef2d378c6cb8e27
Harshit chadha <62325935+harshit0571@users.noreply.github.com>
2023-08-01 10:01:14 +0530

    Create KardaneAlgo.java

---
+++ arrays/KardaneAlgo.java
@@ -0,0 +1,21 @@
+lass Solution{
+
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    long maxSubarraySum(int nums[], int n){
+        int max=Integer.MIN_VALUE;
+        int sum=0;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            max=Math.max(max, sum);
+            
+            if(sum<0){
+                sum=0;
+            }
+        }
+        return max;
+
+    }
+    
+}
