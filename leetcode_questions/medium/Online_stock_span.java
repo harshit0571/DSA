4148c950d33342679384d3fa45ef94f1029d9284
Harshit chadha <62325935+harshit0571@users.noreply.github.com>
2023-08-02 15:36:20 +0530

    Create Online_stock_span.java

---
+++ leetcode_questions/medium/Online_stock_span.java
@@ -0,0 +1,15 @@
+class StockSpanner {
+    Stack<int[]> stack;
+    public StockSpanner() {
+        stack = new Stack<>();
+    }
+
+    public int next(int price) {
+        int span = 1;
+        while(!stack.isEmpty() && stack.peek()[0] <= price){
+            span += stack.pop()[1];
+        }
+        stack.add(new int[]{price, span});
+        return span;
+    }
+}
