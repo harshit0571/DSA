63d302eded567215e62fdacdab56359cd08de647
Harshit chadha <62325935+harshit0571@users.noreply.github.com>
2023-08-03 12:56:27 +0530

    Update Online_stock_span.java

--- leetcode_questions/medium/Online_stock_span.java
+++ leetcode_questions/medium/Online_stock_span.java
@@ -13,3 +13,23 @@ class StockSpanner {
         return span;
     }
 }
+
+
+
+/array
+    class StockSpanner {
+    ArrayList<Integer> arr = new ArrayList<>();    
+    public int next(int price) {
+        arr.add(price);
+        int cnt=0;
+        for(int i=arr.size()-1;i>=0;i--){
+            if(arr.get(i)<=price){
+                cnt++;
+            }
+            else{
+                break;
+            }
+        }
+        return cnt;
+    }
+}
