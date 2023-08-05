71b6522e27e010b7bb20c61fa87ea43aa8c3ecd6
Harshit chadha <62325935+harshit0571@users.noreply.github.com>
2023-08-05 09:39:42 +0530

    Update valid_anagram.java

--- leetcode_questions/easy/valid_anagram.java
+++ leetcode_questions/easy/valid_anagram.java
@@ -79,3 +79,41 @@ class Solution {
 
     }
 }
+
+
+//diff soln using hashmap
+
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        HashMap<Character, Integer> map=new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            if(map.containsKey(s.charAt(i))){
+                map.put(s.charAt(i), map.get(s.charAt(i))+1);
+            }
+            else{
+                map.put(s.charAt(i),1);
+            }
+        }
+
+        for(int i=0;i<t.length();i++){
+            if(map.containsKey(t.charAt(i))){
+                map.put(t.charAt(i), map.get(t.charAt(i))-1);
+            }
+            else{
+                map.put(t.charAt(i),1);
+            }           
+        }
+
+        for(Character i : map.keySet()){
+            if(map.get(i) !=0){
+                return false;
+            }
+        }
+        return true;
+
+    }
+}
